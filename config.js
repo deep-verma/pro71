9b9c3acf783ea43475cb578793862858b45dcc77
deep-verma <75847946+deep-verma@users.noreply.github.com>
2022-12-22 18:45:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,14 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 //Paste your firebaseConfig here
+const firebaseConfig = {
+  apiKey: "AIzaSyCZrok56Md-_GWyq-CuaKTha6iyLK2xNAk",
+  authDomain: "ride-5b72e.firebaseapp.com",
+  projectId: "ride-5b72e",
+  storageBucket: "ride-5b72e.appspot.com",
+  messagingSenderId: "115477134406",
+  appId: "1:115477134406:web:dbbd85a710454966e65e47"
+};
 
 //
 firebase.initializeApp(firebaseConfig);
